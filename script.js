ed82862bf07318fa78788fac9a6734392547fd3d
stelister <samvedvyas@gmail.com>
2024-10-21 19:23:54 +0530

    apply hover effect on grid divs

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const container = document.querySelector(".container");
+
 createGrid(16);
+
+container.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "black";
+})
 
 function createGrid(num_of_squares) {
     for (let i = 0; i < num_of_squares; i++) {
