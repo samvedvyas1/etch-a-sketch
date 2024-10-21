8d2984d4e0e2a35adcaa77eff92cfd91d17bba29
stelister <samvedvyas@gmail.com>
2024-10-21 20:18:06 +0530

    modify grid function such that container size does not change on changing number of squares per side

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const container = document.querySelector(".container");
 
-createGrid(16);
+createGrid(4);
 
 container.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "black";
 })
 
-function createGrid(num_of_squares) {
-    for (let i = 0; i < num_of_squares; i++) {
+function createGrid(size) {
+    let square_size = Math.floor(400 / size);
+    for (let i = 0; i < size; i++) {
         const line = document.createElement("div");
         line.classList.add("line");
         container.appendChild(line);
-        for (let j = 0; j < num_of_squares; j++) {
+        for (let j = 0; j < size; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
+            square.style.height = square.style.width = square_size + "px";
             line.appendChild(square);
         }
     }
